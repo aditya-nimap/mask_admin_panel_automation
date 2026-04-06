a16827953779364fa4ed08508fdada5377d8930b
adityaa-more <adityamore.se@gmail.com>
2026-04-06 18:15:51 +0530

    First push

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,8 +34,6 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'on-first-retry',
-    screenshot: 'on',
-    headless: false
   },
 
   /* Configure projects for major browsers */
