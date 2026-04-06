2fe839ba6de87726f40af22fb010d31686e2b267
adityaa-more <adityamore.se@gmail.com>
2026-04-06 18:24:18 +0530

    login

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
 
-  testIgnore: ['tests/auth/**'],
+//testIgnore: ['tests/auth/**'],
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
